4acb6b2f344fc4fea914f3a2bcbcc040a8101c44
Sergei Ganzhikov <sergei@voidmain.io>
2018-05-03 03:27:30 +0200

    Course2 Week3 Descrete Event Simulation

---
+++ progfun/src/main/scala/week9/Simulation.scala
@@ -0,0 +1,40 @@
+package week9
+
+abstract class Simulation {
+  type Action = () => Unit
+
+  case class Event(time: Int, action: Action)
+
+  private var curtime = 0
+  def currentTime: Int = curtime
+
+  private var agenda: List[Event] = List()
+
+  def afterDelay(delay: Int)(block: => Unit): Unit = {
+    val item = Event(currentTime + delay, () => block)
+    agenda = insert(agenda, item)
+  }
+
+  private def insert(ag: List[Event], item: Event): List[Event] = ag match {
+    case first :: rest if first.time <= item.time =>
+      first :: insert(rest, item)
+    case _ =>
+      item :: ag
+  }
+
+  private def loop(): Unit = agenda match {
+    case first :: rest =>
+      agenda = rest
+      curtime = first.time
+      first.action()
+      loop()
+    case Nil =>
+  }
+
+  def run(): Unit = {
+    afterDelay(0) {
+      println("*** simulation started, time = " + currentTime + " ***")
+    }
+    loop()
+  }
+}
---
+++ progfun/src/main/scala/week9/lecture1.sc
@@ -0,0 +1,7 @@
+import week9.BankAccount
+
+val account = new BankAccount
+account deposit 50
+account withdraw 20
+account withdraw 20
+//account withdraw 15
